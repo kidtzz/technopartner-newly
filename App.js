7f24ce1962e291127b2619744e21149aad6b9a20
kidtzz <bangkitpr4@gmail.com>
2022-03-31 15:52:55 +0700

    first-commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createStore} from 'redux';
+import reducer from './src/reducers';
+import {Provider} from 'react-redux';
+
+import Root from './src/Root';
+
+const store = createStore(reducer);
+
+const App: () => React$Node = () => {
+  return (
+    <Provider store={store}>
+      <Root />
+    </Provider>
+  );
+};
+
+export default App;
